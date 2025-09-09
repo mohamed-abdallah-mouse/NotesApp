da67edd0580e77e7d07d099ede5139b9e4c1efe4
Mohamed Abdallah <mohamed.abdullah.7.2001@gmail.com>
2025-09-09 15:46:36 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/home_vies.dart';
+import 'package:notes_app/views/home_views.dart';
 
 void main() {
   runApp(const NotesApp());
