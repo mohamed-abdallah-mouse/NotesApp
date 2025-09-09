9d363008f7834ce4f1c94f7f96df55fd04fe2951
Mohamed Abdallah <mohamed.abdullah.7.2001@gmail.com>
2025-09-09 15:45:23 +0300

    Add notes Item

---
+++ lib/views/widgets/notes_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(5, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.all(8),
+              title: Text('First Note'),
+              subtitle: Text('Tab to add note'),
+              trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 40, top: 8, bottom: 16),
+              child: Text('31/10/2001'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
