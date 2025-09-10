48763999345507bc51532689d070e91b04cb929b
Mohamed Abdallah <mohamed.abdullah.7.2001@gmail.com>
2025-09-10 17:03:43 +0300

    add Hive

---
+++ lib/models/note_model/note_model.dart
@@ -0,0 +1,22 @@
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  String title;
+  @HiveField(1)
+  String content;
+  @HiveField(2)
+  String data;
+  @HiveField(3)
+  int color;
+
+  NoteModel({
+    required this.title,
+    required this.content,
+    required this.data,
+    required this.color,
+  });
+}
